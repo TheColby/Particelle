4b62d34e75af086411535048b9c18ec097c81665
TheColby <22770687+TheColby@users.noreply.github.com>
2026-06-03 17:52:22 +0000

    Fix cargo fmt on CLI progress render command

--- particelle-cli/src/main.rs
+++ particelle-cli/src/main.rs
@@ -5,8 +5,8 @@ use particelle_core::grain::Cloud;
 use particelle_core::pool::GrainPool;
 use particelle_core::spatializer::AmplitudePanner;
 use particelle_schema::ParticelleConfig;
-use std::io::Read;
 use std::io::IsTerminal;
+use std::io::Read;
 use std::sync::Arc;
 use std::time::Instant;
 
